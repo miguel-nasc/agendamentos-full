b6a87a7d9d29be08ab87bdb710c2a6bc141726ce
Miguel <devmiguelnasc@gmail.com>
2026-08-25 22:39:10 -0300

    ajustando

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,7 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import AppRoutes from './routes/AppRoutes'
 import Login from './pages/Login/Login'
 
